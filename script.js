fed63a38c65a683346f31860321941dd40272c4a
ardhikaxx <ardhikayanuar58@gmail.com>
2024-06-19 07:27:44 +0700

    update- touch

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 import gsap from 'https://cdn.skypack.dev/gsap@3.12.0';
 
-const UPDATE = ({ x, y }) => {
+const UPDATE_MOUSE = ({ x, y }) => {
     gsap.set(document.documentElement, {
         '--x': gsap.utils.mapRange(0, window.innerWidth, -1, 1, x),
         '--y': gsap.utils.mapRange(0, window.innerHeight, -1, 1, y)
     });
-
 };
 
-window.addEventListener('mousemove', UPDATE);
+const UPDATE_TOUCH = ({ touches }) => {
+    const { clientX: x, clientY: y } = touches[0];
+    gsap.set(document.documentElement, {
+        '--x': gsap.utils.mapRange(0, window.innerWidth, -1, 1, x),
+        '--y': gsap.utils.mapRange(0, window.innerHeight, -1, 1, y)
+    });
+};
+
+window.addEventListener('mousemove', UPDATE_MOUSE);
+window.addEventListener('touchmove', UPDATE_TOUCH);
 
 const handleOrientation = ({ beta, gamma }) => {
     const isLandscape = window.matchMedia('(orientation: landscape)').matches;
@@ -16,7 +24,6 @@ const handleOrientation = ({ beta, gamma }) => {
         '--x': gsap.utils.clamp(-1, 1, isLandscape ? gsap.utils.mapRange(-45, 45, -1, 1, beta) : gsap.utils.mapRange(-45, 45, -1, 1, gamma)),
         '--y': gsap.utils.clamp(-1, 1, isLandscape ? gsap.utils.mapRange(20, 70, 1, -1, Math.abs(gamma)) : gsap.utils.mapRange(20, 70, 1, -1, beta))
     });
-
 };
 
 const START = () => {
@@ -34,4 +41,5 @@ const START = () => {
         window.addEventListener("deviceorientation", handleOrientation);
     }
 };
+
 document.body.addEventListener('click', START, { once: true });
